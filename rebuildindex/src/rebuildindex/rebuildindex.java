6a2c7ae370243eb327d2e5c3adb3c185264eea1b
xinchenrealjaja <xin.chen@realjaja.com>
2017-05-13 19:02:29 -0400

    set trytimes = 0 if pictures are not ready

--- rebuildindex/src/rebuildindex/rebuildindex.java
+++ rebuildindex/src/rebuildindex/rebuildindex.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -15,16 +14,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import listing.models.IndexType;
 import models.CommListingBrief;
 import models.ListingBrief;
+import models.PictureInfo;
 import models.ResiListingBrief;
-import play.core.server.noCATrustManager;
 import play.libs.Json;
 import utils.FileUtils;
 
 public class rebuildindex {
 
 	static StringBuilder listingName = new StringBuilder();
-
-	public List<ResiListingBrief> getAllResiListingBriefs(IndexType indexType) {
+	public List<ResiListingBrief> getAllResiListingBriefs (IndexType indexType) {
 		try {
 			String fileName = (indexType == IndexType.Available)
 					? "data/" + listingName.toString() + "/listings/index.txt"
@@ -49,6 +47,172 @@ public class rebuildindex {
 		}
 	}
 
+	
+	public List<ResiListingBrief> getAllResiListings1(List<File> filelist) {
+		try {
+			String fileName ="data/" + listingName.toString() + "/listings/"+filelist.get(0).getName();
+			if (new File(fileName).exists() == false) {
+				System.out.println("didn't find the index.txt, please confirm the path");
+				return new ArrayList<>();
+			}
+			// assuming adapter own index file
+			String listingTxt = getFileContent(fileName);
+			String [] strings = listingTxt.split("\n");
+			Integer attributesStart = 0;
+			Integer attributesEnd = 0;
+			for (int i = 0; i < strings.length; i++) {
+				if (strings[i].contains("attributes")) {
+					attributesStart = i;
+					System.out.println(attributesStart);
+				}
+				if (strings[i].contains("},")) {
+					attributesEnd = i;
+					System.out.println(attributesEnd);
+					break;
+				}
+			}
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i = 0; i < strings.length; i++) {
+				if (i<attributesStart||i>attributesEnd) {
+					stringBuilder.append(strings[i]);
+				}
+			}
+			System.out.println(stringBuilder);
+			//find "attributes" : { and },
+			JsonNode jnode = Json.parse(stringBuilder.toString());
+			
+			ResiListingBrief[] listings = Json.fromJson(jnode, ResiListingBrief[].class);
+
+			List<ResiListingBrief> briefs = new ArrayList<>();
+			for (ResiListingBrief item : listings) {
+				briefs.add(item);
+			}
+			System.out.println(briefs);
+			return briefs;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	public String getNewString(String sTrytimes){
+		Integer start = sTrytimes.indexOf(":")+2;
+		String newString = sTrytimes.substring(0, start)+"0,\n";
+		return newString;
+	}
+	
+	public void setNonPictureListings(List<String> propertyIdList) {
+		for (int j = 0; j < propertyIdList.size(); j++) {
+			try {
+				String fileName ="data/" + listingName.toString() + "/listings/"+propertyIdList.get(j)+".txt";
+				if (new File(fileName).exists() == false) {
+					System.out.println("didn't find the listing file, please confirm the path");
+					continue;
+				}
+				// assuming adapter own index file
+				String listingTxt = getFileContent(fileName);
+				System.out.println(listingTxt);
+				String [] strings = listingTxt.split("\n");
+				Integer attributesStart = 0;
+				StringBuilder stringBuilder = new StringBuilder();
+				for (int i = 0; i < strings.length; i++) {
+					if (strings[i].contains("tryTimes")) {
+						attributesStart = i;
+						//System.out.println(attributesStart);
+						stringBuilder.append(getNewString(strings[i]));
+						
+					}else
+						stringBuilder.append(strings[i]+"\n");
+				}
+				System.out.println(stringBuilder.toString());
+				saveFileContent(fileName, stringBuilder.toString());
+			} catch (Exception ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+	}
+
+	
+	public List<String> getNonPicturesResiListingIds(IndexType indexType) {
+		try {
+			String fileName = (indexType == IndexType.Available)
+					? "data/" + listingName.toString() + "/listings/index.txt"
+					: "data/" + listingName.toString() + "/delistings/30days.txt";
+			if (new File(fileName).exists() == false) {
+				System.out.println("didn't find the index.txt, please confirm the path");
+				return new ArrayList<>();
+			}
+			// assuming adapter own index file
+			String listingTxt = getFileContent(fileName);
+				
+				JsonNode jnode = Json.parse(listingTxt);
+				ResiListingBrief[] listings = Json.fromJson(jnode, ResiListingBrief[].class);
+				
+				List<String> nonPicturesIds = new ArrayList<>();
+				for (ResiListingBrief item : listings) {
+					if (null==item.pictures.fileNames||item.pictures.fileNames.size()==0) {
+						nonPicturesIds.add(item.propertyId);
+					}
+				}
+			return nonPicturesIds;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}	
+	
+	public List<String> getNonPicturesAndTrytimesNotZeroResiIds(IndexType indexType) {
+		try {
+			String fileName = (indexType == IndexType.Available)
+					? "data/" + listingName.toString() + "/listings/index.txt"
+					: "data/" + listingName.toString() + "/delistings/30days.txt";
+			if (new File(fileName).exists() == false) {
+				System.out.println("didn't find the index.txt, please confirm the path");
+				return new ArrayList<>();
+			}
+			// assuming adapter own index file
+			String listingTxt = getFileContent(fileName);
+				
+				JsonNode jnode = Json.parse(listingTxt);
+				ResiListingBrief[] listings = Json.fromJson(jnode, ResiListingBrief[].class);
+				
+				List<String> nonPicturesIds = new ArrayList<>();
+				for (ResiListingBrief item : listings) {
+					if ((null==item.pictures.fileNames||item.pictures.fileNames.size()==0)&&(item.pictures.tryTimes!=0)) {
+						nonPicturesIds.add(item.propertyId);
+					}
+				}
+			return nonPicturesIds;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}	
+	
+	public List<String> getNonPicturesAndTrytimesNotZeroCommIds(IndexType indexType) {
+		try {
+			String fileName = (indexType == IndexType.Available)
+					? "data/" + listingName.toString() + "/listings/indexComm.txt"
+					: "data/" + listingName.toString() + "/delistings/30days.txt";
+			if (new File(fileName).exists() == false) {
+				System.out.println("didn't find the index.txt, please confirm the path");
+				return new ArrayList<>();
+			}
+			// assuming adapter own index file
+			String listingTxt = getFileContent(fileName);
+				
+				JsonNode jnode = Json.parse(listingTxt);
+				ResiListingBrief[] listings = Json.fromJson(jnode, ResiListingBrief[].class);
+				
+				List<String> nonPicturesIds = new ArrayList<>();
+				for (ResiListingBrief item : listings) {
+					if ((null==item.pictures.fileNames||item.pictures.fileNames.size()==0)&&(item.pictures.tryTimes!=0)) {
+						nonPicturesIds.add(item.propertyId);
+					}
+				}
+			return nonPicturesIds;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}	
+	
 	static Map<String, String> indexCache = new HashMap<>();
 
 	static String getFileContent(String fileName) throws Exception {
@@ -196,6 +360,19 @@ public class rebuildindex {
 		String propertyId = map.get("propertyId");
 		if (resiBriefs.containsKey(propertyId)) {
 			System.out.println(FileUtils.toJsonString(resiBriefs.get(propertyId)));
+			Set<String> keySet = map.keySet();
+			Iterator<String> iterator = keySet.iterator();
+			while (iterator.hasNext()) {
+				PictureInfo pictureInfo = resiBriefs.get("pictures").pictures;
+				String field = (String) iterator.next();
+				if (resiBriefs.containsKey(field)) {
+					//not in pictures
+				}else{
+					String [] pictureInfoFields ={"timestamp","tryTimes","fileNames","path","thumb","pictureHash"};
+				}
+				//resiBriefs.get(propertyId).bedRoomExtra = 1;
+				//map.put(field, map.get(field));
+			}
 			resiBriefs.get(propertyId).listingTimestamp = map.get("listingTimestamp");
 
 			System.out.println(FileUtils.toJsonString(resiBriefs.get(propertyId)));
@@ -272,12 +449,50 @@ public class rebuildindex {
 		return updateNum;
 	}
 
+	
+	
+	public int resetTrytimesForNonPictures() throws Exception {
+		int updateNum = 0; 
+		System.out.println("start to change index of " + listingName.toString());
+		List<ResiListingBrief> resiBriefArray = getAllResiListingBriefs(IndexType.Available);
+		Map<String, ResiListingBrief> resiBriefs = toMap(resiBriefArray);
+		System.out.println("Existing resi size:" + resiBriefs.size());
+
+		//List<File> listingFiles = getFileList("data/" + listingName.toString() + "/listings/");
+		//List<ResiListing> resiListingArray = getAllResiListings1(listingFiles);
+		//Map<String, Map<String, String>> resiListingsMap = getAllResiListings(listingFiles);
+		//List<String> nonPicturesIds = getNonPicturesResiListingIds(IndexType.Available);
+		List<String> nonPicturesAndTrytimesNotZeroResiIds = getNonPicturesAndTrytimesNotZeroResiIds(IndexType.Available);
+		setNonPictureListings(nonPicturesAndTrytimesNotZeroResiIds);
+		Iterator<String> iterator = nonPicturesAndTrytimesNotZeroResiIds.iterator();
+		while (iterator.hasNext()) {
+			String nonPicturesId = (String) iterator.next();
+			resiBriefs.get(nonPicturesId).pictures.tryTimes = 0;
+		}
+		//saveResiIndex(resiBriefs.values(), IndexType.Available);
+		System.out.println("end resi index change, start comm index change");
+		List<CommListingBrief> commBriefArray = getAllCommListingBriefs(IndexType.Available);
+		Map<String, CommListingBrief> commBriefs = toMap(commBriefArray);
+		List<String> nonPicturesAndTrytimesNotZeroCommIds = getNonPicturesAndTrytimesNotZeroCommIds(IndexType.Available);
+		setNonPictureListings(nonPicturesAndTrytimesNotZeroCommIds);
+		Iterator<String> iterator1 = nonPicturesAndTrytimesNotZeroCommIds.iterator();
+		while (iterator1.hasNext()) {
+			String nonPicturesId = (String) iterator1.next();
+			commBriefs.get(nonPicturesId).pictures.tryTimes = 0;
+		}
+		//saveCommIndex(commBriefs.values(), IndexType.Available);
+		
+		System.out.println("end to change index");
+		return updateNum;
+	}	
+	
 	public static void main(String[] args) throws Exception {
 		if ((args.length > 0) && null != args) {
 			listingName.append(args[0]);
 			rebuildindex helper = new rebuildindex();
-			// helper.updateIndex(args);
-			helper.updateStyle(args);
+			helper.resetTrytimesForNonPictures();
+			//helper.updateIndex(args);
+			//helper.updateStyle(args);
 		} else {
 			System.out.println("please add parameters");
 		}
