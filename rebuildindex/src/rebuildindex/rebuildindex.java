cec8613b57512c1c978069c9463e643cf1210a71
xinchenrealjaja <xin.chen@realjaja.com>
2017-05-13 22:15:24 -0400

    add compress function, new class

--- rebuildindex/src/rebuildindex/rebuildindex.java
+++ rebuildindex/src/rebuildindex/rebuildindex.java
@@ -1,29 +1,17 @@
 package rebuildindex;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.zip.CRC32;
-import java.util.zip.CheckedOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -596,74 +584,16 @@ public class rebuildindex {
 		System.out.println("backup all files end");
 	}
 
-	static final int BUFFER = 8192;
-    private static File zipFile;
-    public void compress(String srcPathName) {
-        File file = new File(srcPathName);
-        if (!file.exists())
-            throw new RuntimeException(srcPathName + "不存在！");
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
-            CheckedOutputStream cos = new CheckedOutputStream(fileOutputStream, new CRC32());
-            ZipOutputStream out = new ZipOutputStream(cos);
-            String basedir = "";
-            compress(file, out, basedir);
-            out.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    private void compress(File file, ZipOutputStream out, String basedir) {
-        /* 判断是目录还是文件 */
-        if (file.isDirectory()) {
-            //System.out.println("压缩：" + basedir + file.getName());
-            this.compressDirectory(file, out, basedir);
-        } else {
-            //System.out.println("压缩：" + basedir + file.getName());
-            this.compressFile(file, out, basedir);
-        }
-    }
-
-    /** 压缩一个目录 */
-    private void compressDirectory(File dir, ZipOutputStream out, String basedir) {
-        if (!dir.exists())
-            return;
-
-        File[] files = dir.listFiles();
-        for (int i = 0; i < files.length; i++) {
-            /* 递归 */
-            compress(files[i], out, basedir + dir.getName() + "/");
-        }
-    }
-
-    /** 压缩一个文件 */
-    private void compressFile(File file, ZipOutputStream out, String basedir) {
-        if (!file.exists()) {
-            return;
-        }
-        try {
-            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-            ZipEntry entry = new ZipEntry(basedir + file.getName());
-            out.putNextEntry(entry);
-            int count;
-            byte data[] = new byte[BUFFER];
-            while ((count = bis.read(data, 0, BUFFER)) != -1) {
-                out.write(data, 0, count);
-            }
-            bis.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
   	public static void main(String[] args) throws Exception {
 		if ((args.length > 0) && null != args) {
 			listingName.append(args[0]);
 			rebuildindex helper = new rebuildindex();
 			SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
 			System.out.println(df.format(new Date()));
-		    zipFile = new File("data/" + listingName.toString() + "/compressed.zip");
-		    helper.compress("data/" + listingName.toString() + "/backfiles/");
+			Compress.zipFile = new File("data/" + listingName.toString() + "/compressed.zip");
+			Compress compress = new Compress();
+			compress.compress("data/" + listingName.toString() + "/backfiles/");
 		    System.out.println(df.format(new Date()));
 			
 			//helper.autoBackupListings();
